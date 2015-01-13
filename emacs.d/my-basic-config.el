59019a9ee69e80c82d5ad771f2d385506bdc06e2
Mario Mainz <mario.mainz@invision.de>
2015-01-13 15:51:11 +0100

    Change GC memory threshold.

--- emacs.d/my-basic-config.el
+++ emacs.d/my-basic-config.el
@@ -67,7 +67,7 @@
 (electric-pair-mode)
 
 ;; set garbage collection threshold
-(setq gc-cons-threshold 300000000)
+(setq gc-cons-threshold 100000000)
 
 ;; start server for remote calling with emacsclient
 (server-start)
