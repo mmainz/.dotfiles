2ae80cbb8eb5e85949b4437708e08194edb1a6d6
Mario Mainz <mario.mainz@invision.de>
2015-01-18 22:49:23 +0100

    Maximize window on startup.

--- emacs.d/my-basic-config.el
+++ emacs.d/my-basic-config.el
@@ -1,6 +1,9 @@
 ;; don't show welcome screen
 (setq inhibit-startup-message t)
 
+;; maximize window on start
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 ;; Setting rbenv path
 (setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:"
                        (getenv "HOME") "/.rbenv/bin:"
