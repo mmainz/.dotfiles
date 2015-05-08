955cf925b14eec0df4b160700061ffb24cfde569
Mario Mainz <mario.mainz@invision.de>
2015-05-08 16:41:47 +0200

    Create one tag for plain Emacs config and one for Spacemacs.

--- tag-EMACS/emacs.d/init.el
+++ tag-EMACS/emacs.d/init.el
@@ -15,10 +15,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("f0b0710b7e1260ead8f7808b3ee13c3bb38d45564e369cbe15fc6d312f0cd7a0"
-     "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223"
-     "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e"
-     default))))
+    ("26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "f0b0710b7e1260ead8f7808b3ee13c3bb38d45564e369cbe15fc6d312f0cd7a0" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
