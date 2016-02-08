575f04e2c2b86e8b894ebf72ab984008b5853868
Mario Mainz <mario.mainz@invision.de>
2016-02-08 22:37:38 +0100

    Update common Clojure dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[lein-ancient "0.6.7"]]}
- :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "1.2.0"]]
+{:user {:plugins [[lein-ancient "0.6.8"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.10.2"]
+                  [refactor-nrepl "2.0.0"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}
