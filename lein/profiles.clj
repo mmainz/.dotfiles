8852e5e7c757ba98ea8c1c2da6db9f513bc7213f
Mario Mainz <mario.mainz@invision.de>
2016-03-20 14:05:42 +0100

    Update lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.8"]]}
- :repl {:plugins [[cider/cider-nrepl "0.10.2"]
+ :repl {:plugins [[cider/cider-nrepl "0.11.0"]
                   [refactor-nrepl "2.0.0"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}
