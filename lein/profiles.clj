bb04ffa25acab70efbc0cfd6da9a01d41f744fc7
Mario Mainz <mario.mainz@invision.de>
2016-05-13 20:30:15 +0200

    Remove unnecessary Leiningen repl profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,1 @@
-{:user {:plugins [[lein-ancient "0.6.8"]]}
- :repl {:plugins [[cider/cider-nrepl "0.11.0"]
-                  [refactor-nrepl "2.0.0"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
+{:user {:plugins [[lein-ancient "0.6.8"]]}}
